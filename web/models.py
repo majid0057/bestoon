c670073096ef539d5b024bbb0af850b5b0ea24ff
root <majidpv96@gmail.com>
2019-12-27 15:56:30 +0000

    added django .gitignore

--- web/models.py
+++ web/models.py
@@ -9,8 +9,15 @@ class expense(models.Model):
     amount = models.BigIntegerField()
     user = models.ForeignKey(User,on_delete=models.DO_NOTHING)
 
+    def __str__(self):
+        return "{} - {} ".format(self.date, self.amount)
+
 class income(models.Model):
     text = models.CharField(max_length = 255)
     date = models.DateTimeField()
     amount = models.BigIntegerField()
     user = models.ForeignKey(User,on_delete=models.DO_NOTHING)
+
+    def __unicode__(self):
+        return "{} - {} ".format(self.date, self.amount)
+    
